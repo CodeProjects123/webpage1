ae2afef0c2e17d290b9851e29ceac032c04c4fb0
CodeProjects123 <46411900+CodeProjects123@users.noreply.github.com>
2019-01-29 14:38:54 -0600

    added comments

--- models.js
+++ models.js
@@ -1,15 +1,16 @@
-
+//variable
 var slideIndex = 1;
+//call to function
 showSlides(slideIndex);
-
+//functions
 function plusSlides(n) {
+  //calls function
   showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
+  //calls function
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -19,8 +20,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
+  //for loop withing funtion
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active2", "");
+     dots[i].className = dots[i].className.replace(" active2", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active2";
